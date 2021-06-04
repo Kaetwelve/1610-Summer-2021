fcdff1ce4dc8f32cb761c945412edc1d862bf02e
Kade <Kaetwelve@gmail.com>
2021-06-04 11:46:17 -0600

    Script 2

---
+++ New Unity Project (1)/Assets/Scripttwo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scripttwo : MonoBehaviour
+{
+    int myInt = 5;
+
+    void Start ()
+    {
+        myInt = MultiplyByTwo(myInt);
+    }
+
+
+    int MultiplyByTwo (int number)
+    {
+        int ret;
+        ret = number * 2;
+        return ret;
+    }
+}

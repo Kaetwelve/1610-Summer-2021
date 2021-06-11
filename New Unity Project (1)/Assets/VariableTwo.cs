0240b5de90dcd337d52db32fb695fe9e66cfc079
Kade <Kaetwelve@gmail.com>
2021-06-11 15:28:58 -0600

    Variable Two added

---
+++ New Unity Project (1)/Assets/VariableTwo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableTwo : MonoBehaviour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 50 + 100;
+            Console.WriteLine(x);
+        }
+    }
+}

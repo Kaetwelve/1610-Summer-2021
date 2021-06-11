afe651dbea1e9e72a2d6c9559f3dd9f11fb83c2e
Kade <Kaetwelve@gmail.com>
2021-06-11 15:52:50 -0600

    Added Variable Three new

---
+++ New Unity Project (1)/Assets/VariablesThree.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesThree : MonoBehaviour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long myNum = 15000000000L;
+            Console.WriteLine(myNum);
+        }
+    }
+}

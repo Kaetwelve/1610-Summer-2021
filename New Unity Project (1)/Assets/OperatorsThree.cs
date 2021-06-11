1c37a520e9f2ec63e3f66e3f95d760ca3981928a
Kade <Kaetwelve@gmail.com>
2021-06-11 15:41:18 -0600

    fixed names and added Operator 3

---
+++ New Unity Project (1)/Assets/OperatorsThree.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableThree : MonoBehaviour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 50;
+            int y = 2;
+            Console.WriteLine(x/y);
+        }
+    }
+}

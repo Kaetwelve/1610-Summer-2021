b8874fc9086b37d39e36a494a34314fce1887ace
Kade <Kaetwelve@gmail.com>
2021-06-11 15:47:35 -0600

    Adding new Variable one

---
+++ New Unity Project (1)/Assets/Variablesone.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variablesone : MonoBehaviour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            Console.WriteLine(name);
+        }
+    }
+}

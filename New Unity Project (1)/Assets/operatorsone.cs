828db6c40c4f3cb7483a4c99ff8d5de096558396
Kade <Kaetwelve@gmail.com>
2021-06-29 18:46:53 -0600

    blah

--- New Unity Project (1)/Assets/operatorsone.cs
+++ New Unity Project (1)/Assets/operatorsone.cs
@@ -3,15 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+namespace Assets
 {
-    class program
+    public class operatorsone : MonoBehaviour
     {
-        static void Main(string[] args)
+        class program
         {
-            int x = 6;
-            int y = 4;
-            Console.WriteLine(x + y);
+            static void Main(string[] args)
+            {
+                int x = 6;
+                int y = 4;
+                Console.WriteLine(x + y);
+            }
         }
     }
 }

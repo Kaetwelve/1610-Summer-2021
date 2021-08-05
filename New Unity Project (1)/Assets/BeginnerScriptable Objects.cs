9200e29c42c8ca8fae1e796921cd68e9f9ac8500
Kade <Kaetwelve@gmail.com>
2021-08-05 15:56:00 -0600

    Beginner Scritables

---
+++ New Unity Project (1)/Assets/BeginnerScriptable Objects.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LastBitpart1 : MonoBehaviour
+{
+    public float value;
+    public int value;
+
+    public void UpdateValue(float number)
+    {
+        value += number;
+    }
+
+    public void ReplaceValue(float number)
+    {
+        value = number;
+    }
+
+    public void UpdateValue(int number)
+    {
+      value += number
+    }
+    public void ReplaceValue(int number)
+    {
+        value = number
+    }
+}
